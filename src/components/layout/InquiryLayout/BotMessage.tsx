9a736d4ce85a5ca67291a8da870a8c9aebdf15a7
hasangwon <>
2024-10-25 15:57:55 +0900

    Fix : 모바일 짤림 문제 해결

--- src/components/layout/InquiryLayout/BotMessage.tsx
+++ src/components/layout/InquiryLayout/BotMessage.tsx
@@ -11,7 +11,7 @@ const BotMessage = ({
   scrollToBottom: () => void;
 }) => {
   return (
-    <div className="px-[4rem] md:px-[7rem] lg:px-[10rem] overflow-y-auto min-h-[30rem] py-16 text-[24px] font-semibold text-center flex justify-center items-center text-bublitt-text leading-10">
+    <div className="px-[1.5rem] md:px-[7rem] lg:px-[10rem] overflow-y-auto min-h-[30rem] py-16 text-[24px] font-semibold text-center flex justify-center items-center text-bublitt-text leading-10">
       {/* <TypingEffect */}
       <TypingEffectMarkdown
         text={message}
--- src/pages/inquiry.tsx
+++ src/pages/inquiry.tsx
@@ -78,7 +78,7 @@ const Inquiry: React.FC = () => {
               </p>
               <form className="w-full flex flex-col items-center mt-8 gap-6">
                 <div className="flex items-center">
-                  <h3 className="text-[30px] font-semibold mr-12 w-[6.5rem] select-none">
+                  <h3 className="text-[24px] sm:text-[30px] font-semibold sm:mr-12 mr-4 w-[6.5rem] select-none">
                     이름
                   </h3>
                   <input
@@ -94,7 +94,7 @@ const Inquiry: React.FC = () => {
                   />
                 </div>
                 <div className="flex items-center">
-                  <h3 className="text-[30px] font-semibold mr-12 w-[6.5rem] select-none">
+                  <h3 className="text-[24px] sm:text-[30px] font-semibold sm:mr-12 mr-4 w-[6.5rem] select-none">
                     생년월일
                   </h3>
                   <input
@@ -111,7 +111,7 @@ const Inquiry: React.FC = () => {
                   />
                 </div>
                 <div className="flex items-center">
-                  <h3 className="text-[30px] font-semibold mr-12 w-[6.5rem] select-none">
+                  <h3 className="text-[24px] sm:text-[30px] font-semibold sm:mr-12 mr-4 w-[6.5rem] select-none">
                     전화번호
                   </h3>
                   <input
@@ -134,7 +134,7 @@ const Inquiry: React.FC = () => {
                     setPageIndex(1);
                     initUserInfo();
                   }}
-                  className="text-lg px-8 py-2 border border-gentle-light rounded-md mt-[6rem] hover:bg-gentle-light mr-8"
+                  className="text-lg px-8 py-2 border border-gentle-light rounded-md mt-[6rem] hover:bg-gentle-light sm:mr-8 mr-2"
                 >
                   취소
                 </button>
